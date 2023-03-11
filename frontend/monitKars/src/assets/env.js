d324d93a91565d5f990203860c648acacff33b7b
Andrei <andrei.rosu@empact.app>
2023-03-11 21:39:52 +0200

    fromat 2 front code

--- frontend/monitKars/src/assets/env.js
+++ frontend/monitKars/src/assets/env.js
@@ -1,10 +1,10 @@
-(function(window) {
-    window["env"] = window["env"] || {};
-  
-    // Environment variables
-    window["env"]["baseApiUrl"] = 'https://localhost:7083/';
-    window["env"]["autority"] = 'http://localhost:4335';
-    window["env"]["autorityRealm"] = 'master';
-    window["env"]["autorityClientId"] = 'monitKars'
-    window["env"]["scope"] = "openid profile email roles"
-  })(this);
+(function (window) {
+  window["env"] = window["env"] || {};
+
+  // Environment variables
+  window["env"]["baseApiUrl"] = "https://localhost:7083/";
+  window["env"]["autority"] = "http://localhost:4335";
+  window["env"]["autorityRealm"] = "master";
+  window["env"]["autorityClientId"] = "monitKars";
+  window["env"]["scope"] = "openid profile email roles";
+})(this);
--- frontend/monitKars/src/assets/env.template.js
+++ frontend/monitKars/src/assets/env.template.js
@@ -1,10 +1,10 @@
-(function(window) {
-    window["env"] = window["env"] || {};
-  
-    // Environment variables
-    window["env"]["baseApiUrl"] = "${BASE_API_URL}";
-    window["env"]["autority"] = "${AUTORITY}";
-    window["env"]["autorityRealm"] = "${REALM}";
-    window["env"]["autorityClientId"] = "${CLIENT_ID}"
-    window["env"]["scope"] = "${SCOPE}"
-  })(this);
+(function (window) {
+  window["env"] = window["env"] || {};
+
+  // Environment variables
+  window["env"]["baseApiUrl"] = "${BASE_API_URL}";
+  window["env"]["autority"] = "${AUTORITY}";
+  window["env"]["autorityRealm"] = "${REALM}";
+  window["env"]["autorityClientId"] = "${CLIENT_ID}";
+  window["env"]["scope"] = "${SCOPE}";
+})(this);
