b0fc4bb6c58c02a620ddc1e6ea664dbf54dd1d2f
Andrei <andrei.rosu@empact.app>
2023-03-12 20:59:10 +0200

    remove dark theme and notifications toggle

--- frontend/monitKars/src/app/components/addcar/addcar.component.ts
+++ frontend/monitKars/src/app/components/addcar/addcar.component.ts
@@ -1,18 +1,31 @@
 import { Component, OnInit } from '@angular/core';
 import { Car } from 'src/app/models/car.model';
+import { User } from 'src/app/models/user.model';
 import { CarsService } from 'src/app/services/cars/cars.service';
+import { UsersService } from 'src/app/services/users/users.service';
 
 @Component({
   selector: 'app-addcar',
   templateUrl: './addcar.component.html',
   styleUrls: ['./addcar.component.scss'],
 })
-export class AddcarComponent {
+export class AddcarComponent implements OnInit {
   Car = <Car>{};
+  Drivers = <User[]>{};
 
-  constructor(private carService: CarsService) {}
+  constructor(
+    private carService: CarsService,
+    private driverService: UsersService
+  ) {}
 
-  addCar(){
+  addCar() {
+    //Code to get car details from form
     this.carService.postCar(this.Car);
+  }
+
+  ngOnInit(): void {
+    this.driverService.getDrivers().subscribe((res) => {
+      this.Drivers = res;
+    });
   }
 }
