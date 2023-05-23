3fc630d9fc69d2ccf62db672e2be0766a029f936
Andrei <andrei.rosu@empact.app>
2023-05-23 20:36:41 +0300

    add fade in animations and color for statuses

--- frontend/monitKars/src/app/components/car/car.component.ts
+++ frontend/monitKars/src/app/components/car/car.component.ts
@@ -13,15 +13,55 @@ export class CarComponent implements OnInit {
 
   idCar: any = this.route.snapshot.paramMap.get('carID');
 
-  constructor(private carService: CarsService, private route: ActivatedRoute, private router: Router) {}
+  today = new Date();
 
-  editCar(id?: string | null){
+  inspectionDate = Boolean();
+
+  insuranceDate = Boolean();
+
+  vignetteDate = Boolean();
+
+  maintenanceStatus = String();
+
+  constructor(
+    private carService: CarsService,
+    private route: ActivatedRoute,
+    private router: Router
+  ) {}
+
+  editCar(id?: string | null) {
     this.router.navigate(['/editCar', { carID: id }]);
   }
 
   ngOnInit() {
     this.carService.getCar(this.idCar).subscribe((res) => {
       this.car = res;
+      let inspection = new Date(this.car.carPeriodicRevision);
+
+      if (inspection < new Date()) {
+        this.inspectionDate = false;
+      } else {
+        this.inspectionDate = true;
+      }
+
+      let insurance = new Date(this.car.carInsurance);
+
+      if (insurance < new Date()) {
+        this.insuranceDate = false;
+      } else {
+        this.insuranceDate = true;
+      }
+
+      let vignette = new Date(this.car.carVignette);
+
+      if (vignette < new Date()) {
+        this.vignetteDate = false;
+      } else {
+        this.vignetteDate = true;
+      }
+
+      this.maintenanceStatus = this.car.carMaintenanceStatus;
+      console.log(this.maintenanceStatus);
     });
   }
 }
